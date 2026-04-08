53e316fb163fa7a97949be51dfc91f5a0b7590cf
RoyceLando <Roycelando@gmail.com>
2026-04-07 23:14:06 -0400

    go the menue working on my computer v_0.16.0

--- src/main.zig
+++ src/main.zig
@@ -1,55 +1,71 @@
 const std = @import("std");
 const flopie_fish = @import("flopie_fish");
-const io = std.io;
+const Io = std.Io;
+const Init = std.process.Init;
 
 
 // TODO: Refactor the std.out and std.in to make this code look cleaner [2026_04_06]
 // TODO: Understand how the std.in and std.out actually work [2026_04_06]
 
-pub fn menu() !void{
+pub fn menu(init:Init) !void{
     var stdout_buffer:[1024]u8 = undefined;
-    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
-    const stdout = &stdout_writer.interface;
+    var stdout = Io.File.stdout().writer(init.io, &stdout_buffer);
+    const stdout_writer = &stdout.interface;
 
-    try stdout.print("\nWelcom to the flopie fish project! A chess engine written in zig by Royce Lando\n",.{});
-    try stdout.print("=====================================\n",.{});
-    try stdout.print("==             Menu                ==\n",.{});
-    try stdout.print("=====================================\n\n",.{});
-    try stdout.print("1. Play manually\n",.{});
-    try stdout.print("2. Play Flopie Fish\n",.{});
-    try stdout.print("*. Exit\n",.{});
-    try stdout.flush();
 
-    try stdout.print("input: ",.{});
-    var user_input:[]u8 = undefined;
-    var stdin_buffer: [256]u8 = undefined;
-    var stdin_reader = std.fs.File.stdin().reader(&stdin_buffer);
-    const stdin = &stdin_reader.interface;
-    user_input = try stdin.takeDelimiterExclusive('\n');
+    
 
-    const choice:u8 = try std.fmt.parseInt(u8, user_input, 10);
-    //var trimmed_choice = std.mem.trim(u8, choice, &std.ascii.whitespace);
+    
 
-    std.debug.print("choice {} its type is {}\n",.{choice, @TypeOf(choice)});
+   const choice = while (true){
+        try stdout_writer.print("\nWelcom to the flopie fish project! A chess engine written in zig by Royce Lando\n",.{});
+        try stdout_writer.print("=====================================\n",.{});
+        try stdout_writer.print("==             Menu                ==\n",.{});
+        try stdout_writer.print("=====================================\n\n",.{});
+        try stdout_writer.print("1. Play manually\n",.{});
+        try stdout_writer.print("2. Play Flopie Fish\n",.{});
+        try stdout_writer.print("3. Exit\n",.{});
+        try stdout_writer.flush();
+        try stdout_writer.print("input: ",.{});
+        try stdout_writer.flush();
 
-    if(choice == 1){
-        std.debug.print("its equl\n",.{});
+        var stdin_buffer:[1024]u8 = undefined;
+        var stdin = Io.File.stdin().reader(init.io,&stdin_buffer); 
+        var stdin_reader = &stdin.interface;
+
+        const user_input:[]u8 = try stdin_reader.takeDelimiterExclusive(10);
+        const choice:u8 = try std.fmt.parseInt(u8, user_input, 10);
+
+        if(choice == 1 or choice == 2 or choice == 3){
+            break choice;
+        }
+    } else 3;
+
+    switch(choice){
+        1 =>{
+            try stdout_writer.print("You chose to play manually\n",.{});
+            try stdout_writer.flush();
+        },
+        2 =>{
+            try stdout_writer.print("You chose to play flopie\n",.{});
+            try stdout_writer.flush();
+
+        },
+        3=>{
+            try stdout_writer.print("You chose to exit\n",.{});
+            try stdout_writer.flush();
+
+        },
+        else =>{
+            try stdout_writer.print("Invalid option",.{});
+            try stdout_writer.flush();
+        }
     }
 
-    while(choice != 1 and choice != 2 and choice != 3){
-//      try stdout.print("input: ",.{});
-//      try stdout.flush();
-//      user_input = try stdin.takeDelimiterExclusive('\n');
-//      choice = std.mem.trim(u8, user_input, &std.ascii.whitespace);
-//      trimmed_choice = try std.fmt.parseInt(u8, choice, 10);
-
-   }
 
 }
 
-pub fn main() !void {
-    menu() catch |err|{
-        std.debug.print("{}",.{err});
-    };
+pub fn main(init: Init) !void {
+    try menu(init);
 
 }
