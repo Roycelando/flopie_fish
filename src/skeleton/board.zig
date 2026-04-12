5b3b4765115090ad90377fc4e1c6f49a761f337c
Royce Lando <Roycelando@gmail.com>
2026-04-12 03:27:36 -0400

    able to print bits in a board like representation

--- src/skeleton/board.zig
+++ src/skeleton/board.zig
@@ -1,3 +1,4 @@
+const std = @import("std");
 
 const Board = struct {
     wp_bp:u64 = 65280,
@@ -24,3 +25,20 @@ const Board = struct {
     }
 
 };
+
+pub fn printU64Bits(value:u64) void{
+    var i: i7 = 7;
+    while(i>=0):(i -= 1){
+        std.debug.print("{b:0>8}\n",.{(value >> @as(u6,@intCast(8*i))) & 0b11111111});
+    }
+    std.debug.print("\n",.{});
+}
+
+
+pub fn main() !void{
+    const board = Board{};
+
+    printU64Bits(board.bp_bp | board.br_bp | board.bb_bp | board.bn_bp | board.bq_bp | board.bk_bp);
+    printU64Bits(board.wp_bp | board.wr_bp | board.wb_bp | board.wn_bp | board.wq_bp | board.wk_bp);
+
+}
