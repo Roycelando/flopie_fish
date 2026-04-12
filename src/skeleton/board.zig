13e6f3eeabbc49d8b90ae363c337f5459d03d90a
Royce Lando <Roycelando@gmail.com>
2026-04-11 20:09:57 -0400

    started writing the board logic

---
+++ src/skeleton/board.zig
@@ -0,0 +1,26 @@
+
+const Board = struct {
+    wp_bp:u64 = 65280,
+    wr_bp:u64 = 129,
+    wn_bp:u64 = 66,
+    wb_bp:u64 = 36,
+    wq_bp:u64 = 8,
+    wk_bp:u64 = 16,
+
+
+    bp_bp:u64 = 71776119061217280,
+    br_bp:u64 = 9295429630892703744,
+    bn_bp:u64 = 4755801206503243776,
+    bb_bp:u64 = 2594073385365405696,
+    bq_bp:u64 = 576460752303423488,
+    bk_bp:u64 = 1152921504606846976,
+
+    fn initBoardFromU64Array() void{
+        return;
+    }
+
+    fn initBoardFromU64() void{
+        return;
+    }
+
+};
