9d4b66079fff605ee88ddb9b17c4fc4694c209fb
Royce Lando <Roycelando@gmail.com>
2026-04-19 17:11:39 -0400

    started writing logic for legal pawn moves

--- src/root.zig
+++ src/root.zig
@@ -1,11 +1,39 @@
 const board_file = @import("skeleton/board.zig");
 const piece_file = @import("skeleton/piece.zig");
+const chess_rules = @import("brain/chess_rules.zig");
 
-pub const printU64Bits = board_file.printU64Bits;
-pub const printAllU64BitBoards = board_file.printAllU64BitBoards;
+//==========================
+// Types
+//==========================
+
+//board.zig types
 pub const Board = board_file.Board;
 
+//piece.zig types
 pub const  Piece = piece_file.Piece;
 pub const  Color = piece_file.Color;
+
+//==========================
+// Errors
+//==========================
+
+//board.zig types
+pub const MoveError = board_file.MoveError;
+
+
+//==========================
+// Functions
+//==========================
+
+// board.zig functions
+pub const printU64Bits = board_file.printU64Bits;
+pub const printAllU64BitBoards = board_file.printAllU64BitBoards;
+pub const printAsciiBaord = board_file.printAsciiBoard;
+
+// piece.zig functions
 pub const pieceValue = piece_file.pieceValue;
+
+//chess_rules.zig functions
+pub const isLegalPawnMove = chess_rules.isPawnMoveLegal;
+//
 
